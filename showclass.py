67ddacc0abcfa0c394f10917d1470607983d6b68
XueDugu <113182164+XueDugu@users.noreply.github.com>
2023-12-27 02:02:45 +0800

    Update showclass.py

--- showclass.py
+++ showclass.py
@@ -1,11 +1,20 @@
 from com.pnfsoftware.jeb.client.api import IScript
+from com.pnfsoftware.jeb.client.api import IGraphicalClientContext
 from com.pnfsoftware.jeb.core import RuntimeProjectUtil
 from com.pnfsoftware.jeb.core.units import IUnit
-from com.pnfsoftware.jeb.core.units.code import ICodeUnit
 from com.pnfsoftware.jeb.core.units.code.android import IDexUnit
+from org.eclipse.swt import SWT
+from org.eclipse.swt.layout import GridLayout
+from org.eclipse.swt.widgets import Display, Shell, Text
 
-class ShowClassNamesScript(IScript):
+class ShowClassNamesPlugin(IScript):
     def run(self, ctx):
+        # 获取当前 JEB 客户端上下文
+        clientContext = ctx.getGraphicalClientContext()
+        if not isinstance(clientContext, IGraphicalClientContext):
+            print("This script requires a graphical client context.")
+            return
+
         # 获取当前活动的 JEB 项目
         prj = ctx.getMainProject()
         if prj is None:
@@ -18,23 +27,39 @@ class ShowClassNamesScript(IScript):
             print("No DEX units found")
             return
 
-        # 遍历 Dex 单元并显示所有类名
+        # 获取所有类名
+        class_names = []
         for dex_unit in dex_units:
-            print("Classes in DEX unit: %s" % dex_unit.getName())
-            self.show_class_names(dex_unit)
+            code_unit = dex_unit.getCodeItem()
+            if isinstance(code_unit, IUnit):
+                classes = code_unit.getClasses()
+                for class_info in classes:
+                    class_names.append(class_info.getName())
 
-    def show_class_names(self, dex_unit):
-        # 获取代码单元
-        code_unit = dex_unit.getCodeItem()
-        if not isinstance(code_unit, ICodeUnit):
-            print("Not a valid code unit")
-            return
+        # 创建显示对象
+        display = Display()
 
-        # 获取类名列表
-        classes = code_unit.getClasses()
-        for class_info in classes:
-            print("Class Name: %s" % class_info.getName())
+        # 创建 shell（窗口）
+        shell = Shell(display, SWT.SHELL_TRIM | SWT.BORDER)
+        shell.setLayout(GridLayout())
+        shell.setText("JEB Class Names")
+
+        # 在 shell 中添加文本框，显示所有类名
+        text = Text(shell, SWT.MULTI | SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL)
+        for class_name in class_names:
+            text.append(class_name + "\n")
+
+        # 打开 shell
+        shell.open()
+
+        # 运行直到 shell 关闭
+        while not shell.isDisposed():
+            if not display.readAndDispatch():
+                display.sleep()
+
+        # 释放资源
+        display.dispose()
 
 # 创建脚本实例并运行
-script = ShowClassNamesScript()
+script = ShowClassNamesPlugin()
 script.run(currentState)
